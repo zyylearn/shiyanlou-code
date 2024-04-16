e3b623e4d349361b3efd5702bd52ce4cceaa0a2f
zyylearn <zyyuniverseocean@163.com>
2024-04-16 09:27:08 +0000

    ‘第一次提交’

---
+++ jump7.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+# Write Python 2 code in this online editor and run it.
+#!/usr/bin/python
+for x in range(1,100):
+	if (x%7== 0):
+    		continue
+            	if(x%10==7):
+                		continue
+                        	if (x%10==0):
+                            		continue
+                                    	else:
+                                        		print(x)
+
+
+
+
+
+
